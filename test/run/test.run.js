90be2e7d8085f841b5182ef6838b9230cda527d0
asamuzaK (Kazz) <kazz@asamuzak.jp>
2016-07-21 17:05:25 +0900

    Update test

--- test/run/test.run.js
+++ test/run/test.run.js
@@ -52,6 +52,7 @@ describe("fx-runner start", function () {
         expect(stderr).to.not.be.ok;
         expect(stdout).to.contain("-test");
         expect(stdout).to.contain("testArg");
+        expect(stdout).to.not.contain("-test testArg");
         expect(stdout).to.not.contain("--binary-args");
         expect(stdout).to.not.contain("--profile");
         expect(stdout).to.not.contain("--P");
